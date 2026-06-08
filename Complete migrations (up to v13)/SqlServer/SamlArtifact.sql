e243e041bc9a663299e030e52434f08be086d9ce
NachoEscrig <nacho.escrig@rocksolidknowledge.com>
2026-06-08 13:13:31 +0100

    migrations up to v13

---
+++ Complete migrations (up to v13)/SqlServer/SamlArtifact.sql
@@ -0,0 +1,29 @@
+﻿IF OBJECT_ID(N'[__EFMigrationsHistory]') IS NULL
+BEGIN
+    CREATE TABLE [__EFMigrationsHistory] (
+        [MigrationId] nvarchar(150) NOT NULL,
+        [ProductVersion] nvarchar(32) NOT NULL,
+        CONSTRAINT [PK___EFMigrationsHistory] PRIMARY KEY ([MigrationId])
+    );
+END;
+GO
+
+BEGIN TRANSACTION;
+CREATE TABLE [SamlArtifacts] (
+    [Key] nvarchar(200) NOT NULL,
+    [EntityId] nvarchar(200) NOT NULL,
+    [MessageType] nvarchar(50) NOT NULL,
+    [Message] nvarchar(max) NOT NULL,
+    [CreationTime] datetime2 NOT NULL,
+    [Expiration] datetime2 NOT NULL,
+    CONSTRAINT [PK_SamlArtifacts] PRIMARY KEY ([Key])
+);
+
+CREATE INDEX [IX_SamlArtifacts_Expiration] ON [SamlArtifacts] ([Expiration]);
+
+INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+VALUES (N'20220114164649_SqlServerSamlArtifactInitialMigration', N'10.0.8');
+
+COMMIT;
+GO
+
---
+++ Complete migrations (up to v13)/SqlServer/WsFederationConfiguration.sql
@@ -0,0 +1,40 @@
+﻿IF OBJECT_ID(N'[__EFMigrationsHistory]') IS NULL
+BEGIN
+    CREATE TABLE [__EFMigrationsHistory] (
+        [MigrationId] nvarchar(150) NOT NULL,
+        [ProductVersion] nvarchar(32) NOT NULL,
+        CONSTRAINT [PK___EFMigrationsHistory] PRIMARY KEY ([MigrationId])
+    );
+END;
+GO
+
+BEGIN TRANSACTION;
+CREATE TABLE [RelyingParties] (
+    [Id] int NOT NULL IDENTITY,
+    [Realm] nvarchar(200) NOT NULL,
+    [TokenType] nvarchar(max) NULL,
+    [SignatureAlgorithm] nvarchar(max) NULL,
+    [DigestAlgorithm] nvarchar(max) NULL,
+    [SamlNameIdentifierFormat] nvarchar(max) NULL,
+    CONSTRAINT [PK_RelyingParties] PRIMARY KEY ([Id])
+);
+
+CREATE TABLE [RelyingPartyClaimMappings] (
+    [Id] int NOT NULL IDENTITY,
+    [OriginalClaimType] nvarchar(250) NOT NULL,
+    [NewClaimType] nvarchar(250) NOT NULL,
+    [RelyingPartyId] int NOT NULL,
+    CONSTRAINT [PK_RelyingPartyClaimMappings] PRIMARY KEY ([Id]),
+    CONSTRAINT [FK_RelyingPartyClaimMappings_RelyingParties_RelyingPartyId] FOREIGN KEY ([RelyingPartyId]) REFERENCES [RelyingParties] ([Id]) ON DELETE CASCADE
+);
+
+CREATE UNIQUE INDEX [IX_RelyingParties_Realm] ON [RelyingParties] ([Realm]);
+
+CREATE INDEX [IX_RelyingPartyClaimMappings_RelyingPartyId] ON [RelyingPartyClaimMappings] ([RelyingPartyId]);
+
+INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+VALUES (N'20190305133618_SqlServerWsfederationInitial', N'10.0.8');
+
+COMMIT;
+GO
+
